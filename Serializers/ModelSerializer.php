b7c03577a90116066af80dd4edc4954f8e576712
lonefox <cris78geo@gmail.com>
2021-03-11 11:27:53 -0600

    oops

--- Serializers/ModelSerializer.php
+++ Serializers/ModelSerializer.php
@@ -1,18 +1,26 @@
 <?php
 
 namespace Djaravel\Serializers;
+use \Djravel\Models\Model;
 
 class ModelSerializer{
 	public $data;
 	function __construct($objects){
-		$data = json_encode(
-			array_map(
-				function($obj){
-					return $obj->serialize();
-				},
-				$objects
-			)
-		);
-		$this->data = $data;
+		if (is_array($objects)) {
+			$data = json_encode(
+				array_map(
+					function($obj){
+						return $obj->serialize();
+					},
+					$objects
+				)
+			);
+			$this->data = $data;
+		}elseif ($objects instanceof Model) {
+			# code...
+		}else{
+			throw new InvalidArgumentException('The constructor only accepts instances of Model or an array of instances.');
+			
+		}
 	}
 }
