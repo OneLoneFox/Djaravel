cd8ccf85c688ba6c1d97a92606d35089d1f5b6f0
lonefox <cris78geo@gmail.com>
2021-03-11 12:01:55 -0600

    fixes incomplete class issue (fuck php man)

--- Bootstrap.php
+++ Bootstrap.php
@@ -12,10 +12,6 @@ $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__."/../");
 $dotenv->load();
 $dotenv->required(['BASE_DIR', 'DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DEBUG']);
 
-# Initialize session
-session_start();
-
-
 spl_autoload_register(function($class){
 	$base_dir = __DIR__."/../".DIRECTORY_SEPARATOR;
 	$file = $base_dir . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
@@ -24,3 +20,6 @@ spl_autoload_register(function($class){
 		require $file;
 	}
 });
+
+# Initialize session
+session_start();
